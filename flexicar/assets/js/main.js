fdde03d889ed4ce3a71a5c24ac205d54df7edbaa
benjamin <strictly4eternity@gmail.com>
2017-06-01 03:20:16 +0100

    Flexicar front end
    flexicar front end design

---
+++ flexicar/assets/js/main.js
@@ -0,0 +1,32 @@
+﻿
+   var length = $('#left-wrap').height() - $('.sidebar').height() + $('#left-wrap').offset().top;
+
+    $(window).scroll(function () {
+
+        var scroll = $(this).scrollTop();
+        var height = $('.sidebar').height() + 'px';
+
+        if (scroll < $('#left-wrap').offset().top) {
+
+            $('.sidebar').css({
+                'position': 'absolute',
+                'top': '0'
+            });
+
+        } else if (scroll > length) {
+
+            $('.sidebar').css({
+                'position': 'absolute',
+                'bottom': '0',
+                'top': 'auto'
+            });
+
+        } else {
+
+            $('.sidebar').css({
+                'position': 'fixed',
+                'top': '0',
+                'height': height
+            });
+        }
+    });
